37a1c8c0703933fe7415571fa5cd9de64c2be23c
petermunnings <peter@munnings.co.za>
2014-12-09 18:58:27 +0200

    Hide if no church events

--- Oikonomos/oikonomos/oikonomos/Scripts/Site/Index.js
+++ Oikonomos/oikonomos/oikonomos/Scripts/Site/Index.js
@@ -2,7 +2,9 @@
 
 $(document).ready(function () {
 
-    $("#accordion").accordion();
+    $("#accordionIndex").accordion({
+        heightStyle: "content"
+    });
 
     $("#SelectedChurchId").change(function () {
 
@@ -17,9 +19,21 @@ $(document).ready(function () {
 
     $.get("/Ajax/FetchChurchEvents", function(data) {
         $("#churchEventsList").empty();
-        $("#churchEventsTemplate")
+        if (data.ChurchEvents.length == 0) {
+            var $accordion = $("#accordionIndex").accordion();
+            var current = $accordion.accordion("option", "active"),
+                maximum = $accordion.find("h3").length,
+                next = current + 1 === maximum ? 0 : current + 1;
+                // $accordion.accordion("activate",next); // pre jQuery UI 1.10
+            $accordion.accordion("option", "active", next);
+            $("#churchEventsTab").next().hide();
+            $("#churchEventsTab").hide();
+        } else {
+            $("#churchEventsTemplate")
                 .tmpl(data.ChurchEvents)
                 .appendTo("#churchEventsList");
+        }
+
     });
     
 
